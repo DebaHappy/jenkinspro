15594f3208d0b204a05a555015f9d9981196e2ae
DELL <DELL@DESKTOP-79R0UBN>
2022-08-28 14:59:31 +0530

    New one

---
+++ src/test/java/Scripts/Test6.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 
+{
+	@Test
+	public void test6() 
+	{
+		Reporter.log("Hi Happy0707",true);
+	}
+}
