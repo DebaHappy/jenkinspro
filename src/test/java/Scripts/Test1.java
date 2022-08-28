33c8ba3361118cf7dbefc4bc241ddfb3e9db5f15
DELL <DELL@DESKTOP-79R0UBN>
2022-08-28 14:19:28 +0530

    Hi jenkins

---
+++ src/test/java/Scripts/Test1.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void test1() 
+	{
+		Reporter.log("Hi debashish",true);
+	}
+}
---
+++ src/test/java/Scripts/Test2.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+	public void test1() 
+	{
+		Reporter.log("Hi Happy",true);
+	}
+}
