e776fd164fa299c8fd9239e975b98dfc62012bf4
DELL <DELL@DESKTOP-79R0UBN>
2022-08-28 15:00:51 +0530

    last one

---
+++ src/test/java/Scripts/Test7.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test7 
+{
+	@Test
+	public void test7() 
+	{
+		Reporter.log("Hi Happy0707Sujit",true);
+	}
+}
