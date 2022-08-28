786b1f51c2bbf4b967a8ac395473d5ef4e042e70
DELL <DELL@DESKTOP-79R0UBN>
2022-08-28 14:56:29 +0530

    New code

---
+++ src/test/java/Scripts/Test4.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 
+{
+	@Test
+	public void test4() 
+	{
+		Reporter.log("Hi there",true);
+	}
+}
---
+++ src/test/java/Scripts/Test5.java
@@ -0,0 +1,13 @@
+package Scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5
+{
+	@Test
+	public void test5() 
+	{
+		Reporter.log("Hi sir",true);
+	}
+}
